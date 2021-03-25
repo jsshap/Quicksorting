fb9fadace3874acc145a3a9c0450227bd89ef957
jsshap <jsshap@gmail.com>
2021-03-25 09:09:33 -0400

    sorting works now

--- Quicksort.java
+++ Quicksort.java
@@ -1,35 +1,23 @@
 import java.util.Random;
-/*
-THIS DOESN't WORK
-
-*/
 
 
 public class Quicksort{
 
     public static void main (String[] args){
         Generate g = new Generate();
-        int[] toSort= g.generateRandomInput(4);
+        int[] toSort= g.generateRandomInput(10000);
 
         quicksort(toSort, 0, toSort.length-1);
         System.out.println(isSorted(toSort));
-
-        for (int i= 0; i< toSort.length; i++){
-            System.out.println(toSort[i]/1000);
-        }
 
     }
 
     public static void quicksort(int[] a, int lo, int hi){
         Random rand = new Random();
-        int[] toSort=a;
-        for (int i= 0; i< toSort.length; i++){
-            System.out.println(toSort[i]/1000);
-        }
-        System.out.println("---");
-        if (hi>lo){
-            int r = rand.nextInt(hi - lo) + lo;//this gives r between hi and lo
 
+        if (hi>lo){
+            int r = rand.nextInt(hi - lo + 1 ) + lo;//this gives r between hi and lo
+            //int r = (hi+lo)/2;
             int p = partition(a, lo, hi, r);
             quicksort(a, lo, p-1);//We never have p = lo
             quicksort(a, p+1, hi);
@@ -37,22 +25,50 @@ public class Quicksort{
     }
 
     public static int partition(int[] a, int lo, int hi, int r){
+        //System.out.println("line 38");
+        //printArray(a);
+        //System.out.println("r is "+ r );
+        //System.out.println("Swapping a[r]= "+ a[r]/1000+ " and a[hi]=" + a[hi]/1000);
         swap(a, r, hi);
+        //System.out.println("line 39");
+        //printArray(a);
         int j = hi-1;
         int i = lo;
-        int x = a[hi];
-
-        while (i<j){
-            if (a[i]<x){//belongs to smalls
+        int x = a[hi];//x is the pivot value. this was a[r]
+        //System.out.println("x="+x/1000);
+        while (i<=j){
+            if (a[i]<=x){//belongs to smalls
+                //System.out.println(a[i]/1000 + "is less than " + x/1000);
                 i++;
             }
             else{//belongs to smalls
+                //System.out.println("Swapping a[i]=" + a[i]/1000 + " with a[j]=" + a[j]/1000);
+                
+                //System.out.println("Pre swap");
+                //printArray(a);
                 swap(a, i, j);
+                //System.out.println("Swap is done");
+                //printArray(a);
                 j--;
             }
         }
-        swap(a, hi, j);
-        return j;//the new location of x
+        /*System.out.println("now the array is: ");
+        printArray(a);
+        System.out.println("Swapping hi and j+1");*/
+        swap(a, hi, j+1);
+        /*System.out.println("That swap is done");
+        printArray(a);
+        System.out.println("J+1 is "+ (j+1) + "a[j+1] is " + a[j+1]);*/
+        return j+1;//the new location of x
+
+    }
+
+    static void printArray(int[] a){
+        int[] toSort=a;
+        for (int i= 0; i< toSort.length; i++){
+            System.out.println(toSort[i]/1000);
+        }
+        System.out.println("====");
 
     }
 
