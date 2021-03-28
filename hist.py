55c428aa27619dd6a3e41d4a60e0f5e533bf005e
jsshap <jsshap@gmail.com>
2021-03-28 16:42:06 -0400

    all graphs done

--- hist.py
+++ hist.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('deterministc_partial_HistV2.csv', usecols=[1])
+data = pd.read_csv('random_mostly_HistV2.csv', usecols=[5])
 
 data.plot(kind='density')
 plt.ylabel('Frequency')
 plt.xlabel('Number of Comparisons')
-plt.title('Partially Sorted Deterministic Larger Number of Elements')
+plt.title('Randomized Quicksort on Mostly Sorted Inputs for More Elements')
 
 ax=plt.gca()
 #ax.get_yaxis().get_major_formatter().set_scientific(False)
