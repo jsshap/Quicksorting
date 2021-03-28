408b131b0ecf4f1b703fd4ffa32f0fc4370cef2a
jsshap <jsshap@gmail.com>
2021-03-28 16:46:21 -0400

    changed filesystem structure

--- transpose.py
+++ transpose.py
@@ -1,3 +1,4 @@
 
 import pandas as pd
 pd.read_csv('deterministic_random_Hist.csv', header=None).T.to_csv('deterministc_random_HistV2.csv', header=False, index=False)
+#https://stackoverflow.com/questions/4869189/how-to-transpose-a-dataset-in-a-csv-file
